f0abd5843458a5f88dc8d59dfbc447ed5d233216
arlenk <arlen.khodadadi@gmail.com>
2018-04-03 23:11:41 -0400

    fix term count vs. word count metrics

--- tmeval/corpora/metrics.py
+++ tmeval/corpora/metrics.py
@@ -2,11 +2,30 @@
 Corpus related metrics
 
 """
+from collections import Counter
+
 import numpy as np
 from gensim.interfaces import CorpusABC
 
 
-def words_per_document(corpus: CorpusABC) -> np.ndarray:
+def term_count_per_document(corpus: CorpusABC) -> np.ndarray:
+    """
+    Measure distribution of number of terms (unique words) per document
+
+    :param corpus: corpus to measure metric on
+    :return: number of terms per document
+
+    """
+    number_of_terms = []
+
+    for doc in corpus:
+        number_of_terms.append(len(doc))
+
+    number_of_terms = np.array(number_of_terms)
+    return number_of_terms
+
+
+def word_count_per_document(corpus: CorpusABC) -> np.ndarray:
     """
     Measure distribution of number of words per document
 
@@ -17,7 +36,11 @@ def words_per_document(corpus: CorpusABC) -> np.ndarray:
     number_of_words = []
 
     for doc in corpus:
-        number_of_words.append(len(doc))
+        document_word_count = 0
+        for term, count in doc:
+            document_word_count += count
+        number_of_words.append(document_word_count)
 
     number_of_words = np.array(number_of_words)
     return number_of_words
+
