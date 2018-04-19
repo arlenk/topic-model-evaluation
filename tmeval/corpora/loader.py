e8a15e869817f398bc493b5ceee4069b24b5ce1a
arlenk <arlen.khodadadi@gmail.com>
2018-04-18 21:38:43 -0400

    load dictionary along with corpus

--- tmeval/corpora/loader.py
+++ tmeval/corpora/loader.py
@@ -3,23 +3,32 @@ Load previously generated/downloaded corpora
 
 """
 from pathlib import Path
-from gensim.corpora import MmCorpus
+from gensim.corpora import MmCorpus, Dictionary
 
 
 def load(corpus_name: str,
-         data_path : Path) -> MmCorpus:
+         data_path: Path) -> (MmCorpus, Dictionary):
     """
     Load corpus by name
 
     :param corpus_name: str
     :param data_path: Path
-    :return: Corpus
+    :return: (MmCorpus, Dictionary)
+
     """
     corpus_file = data_path / "{}.mm".format(corpus_name)
+    dictionary_file = data_path / "{}.dictionary".format(corpus_name)
+
     if not corpus_file.exists():
         raise ValueError("unknown corpus: {}, "
                          "looking for file: {}".format(corpus_name, corpus_file))
 
+    if not dictionary_file.exists():
+        raise ValueError("unknown dictionary: {}, "
+                         "looking for file: {}".format(dictionary_name, dictionary_file))
+
     corpus = MmCorpus(str(corpus_file))
-    return corpus
+    dictionary = Dictionary.load(str(dictionary_file))
+
+    return corpus, dictionary
 
