0f42e93bded7be39f598075580e045851598027e
arlenk <arlen.khodadadi@gmail.com>
2018-04-19 22:12:34 -0400

    split UCI corpora into training & validation corpus files

--- tmeval/corpora/generate/uci.py
+++ tmeval/corpora/generate/uci.py
@@ -6,12 +6,72 @@ https://archive.ics.uci.edu/ml/datasets.html
 """
 import urllib
 import os
-import gensim.corpora as gc
+from itertools import islice
+
 import copy
 from gensim.models import VocabTransform
+from gensim.corpora.dictionary import Dictionary
+from gensim.corpora import MmCorpus, UciCorpus
+from gensim.interfaces import CorpusABC
 
 
-def download_corpus(corpus_name, target_path):
+def generate_mmcorpus_files(corpus_name: str,
+                            target_path: str,
+                            training_pct: float = .8):
+    """
+    Output training and validation MM corpus files
+    :param corpus_name:
+    :param target_path:
+    :param training_pct:
+    :return:
+
+    """
+    corpus = download_corpus(corpus_name, target_path)
+    print("downloaded {} corpus [num_docs={}, num_terms={}]".format(corpus_name,
+                                                                    corpus.num_docs,
+                                                                    corpus.num_terms))
+
+    print("dropping top/bottom words in dictionary")
+    corpus, dictionary = filter_corpus(corpus)
+
+    # output mm files
+    num_documents = len(corpus)
+    num_documents_training = int(training_pct * num_documents)
+    num_documents_validation = num_documents - num_documents_training
+    num_vocab = len(dictionary)
+
+    print("outputting")
+    print("  num documents: {:,.0f}".format(num_documents))
+    print("  num training: {:,.0f}".format(num_documents_training))
+    print("  num validation: {:,.0f}".format(num_documents_validation))
+    print("  vocab size: {:,.0f}".format(num_vocab))
+
+    # output same dictionary for training and validation
+    output_prefix = os.path.join(target_path, "{}.filtered".format(corpus_name))
+    dictionary_file = output_prefix + ".dictionary"
+    dictionary.save(dictionary_file)
+
+    # training data
+    output_file = output_prefix + ".training.mm"
+    training_corpus = islice(corpus, num_documents_training)
+    MmCorpus.serialize(output_file, training_corpus, dictionary)
+
+    # validation
+    output_file = output_prefix + ".validation.mm"
+    validation_corpus = islice(corpus, num_documents_training, num_documents)
+    MmCorpus.serialize(output_file, validation_corpus, dictionary)
+
+
+def download_corpus(corpus_name: str,
+                    target_path: str) -> UciCorpus:
+    """
+    Download corpus from UCI website
+
+    :param corpus_name:
+    :param target_path:
+    :return:
+    """
+
     url_root = "https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/"
     target_path = os.path.join(target_path, "uci", "raw")
     if not os.path.exists(target_path):
@@ -28,26 +88,27 @@ def download_corpus(corpus_name, target_path):
     urllib.request.urlretrieve(url_root + "docword.{}.txt.gz".format(corpus_name),
                                filename=docword_file)
 
-    corpus = gc.UciCorpus(docword_file, vocab_file)
+    corpus = UciCorpus(docword_file, vocab_file)
     return corpus
 
 
-def create_corpus(corpus_name, target_path):
-    corpus = download_corpus(corpus_name, target_path)
+def filter_corpus(corpus: UciCorpus) -> (CorpusABC, Dictionary):
+    """
+    Filter extreme (frequent and infrequent) words from dictionary
 
-    print("dropping top/bottom words in dictionary")
+    :param corpus:
+    :return: (filtered corpus, filtered dictionary)
+    """
 
-    # filter the dictionary
-    old_dict = corpus.create_dictionary()
-    new_dict = copy.deepcopy(old_dict)
-    new_dict.filter_extremes(no_below=20, no_above=.1)
-    filtered_dictionary_file = os.path.join(target_path, "{}.filtered.dictionary".format(corpus_name))
-    new_dict.save(filtered_dictionary_file)
+    # filter dictionary first
+    original_dict = corpus.create_dictionary()
+    filtered_dict = copy.deepcopy(original_dict)
+    filtered_dict.filter_extremes(no_below=20, no_above=.1)
 
     # now transform the corpus
-    old2new = {old_dict.token2id[token]: new_id for new_id, token in new_dict.iteritems()}
+    old2new = {original_dict.token2id[token]: new_id for new_id, token in filtered_dict.iteritems()}
     vt = VocabTransform(old2new)
-    filtered_corpus_file = os.path.join(target_path, "{}.filtered.mm".format(corpus_name))
-    gc.MmCorpus.serialize(filtered_corpus_file, vt[corpus], id2word=new_dict)
+
+    return vt[corpus], filtered_dict
 
 
